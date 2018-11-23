efdb7ca3dcebc563839f10858d83a62696cbe0c3
Seungjae Park <astro.psj@gmail.com>
2018-11-23 23:35:04 +0900

    Change app theme color and add music player activity

--- app/src/main/java/me/astro36/musicai/MusicRecyclerViewAdapter.kt
+++ app/src/main/java/me/astro36/musicai/MusicRecyclerViewAdapter.kt
@@ -9,21 +9,21 @@ import android.widget.ImageView
 import android.widget.TextView
 
 class MusicRecyclerViewAdapter(val context: Context, val musicList: ArrayList<Music>) :
-    RecyclerView.Adapter<MusicRecyclerViewAdapter.Holder>() {
+    RecyclerView.Adapter<MusicRecyclerViewAdapter.ViewHolder>() {
     override fun getItemCount(): Int {
         return musicList.size
     }
 
-    override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.bind(musicList[position], context)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.bind(musicList[position], context)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.item_music, parent, false)
-        return Holder(view)
+        return ViewHolder(view)
     }
 
-    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val musicThumbnail = itemView.findViewById<ImageView>(R.id.music_thumbnail)
         val musicTitle = itemView.findViewById<TextView>(R.id.music_title)
         val musicDetails = itemView.findViewById<TextView>(R.id.music_details)
